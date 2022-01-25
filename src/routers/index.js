27fafce770b92f5f7fa4142fc7e72262c7d142d6
scraneg <3244856228@qq.com>
2022-01-26 02:02:40 +0800

    init

---
+++ src/routers/index.js
@@ -0,0 +1,24 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+const routes = [
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('@/views/login/login')
+    },
+    {
+        path: '/register',
+        component: ()=>import('@/views/register/register')
+    },
+    {
+        path: '/reader',
+        component:()=>import('@/views/reader/reader')
+    }
+]
+
+const history = createWebHistory()
+const router = createRouter({
+    history, // 路由模式
+    routes,
+})
+export default router
